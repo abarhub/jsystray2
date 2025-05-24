b96f285a7a5f59e0b8ad5ec2be06f2a34bfe7a55
abarhub <abarhub@gmail.com>
2025-05-24 17:13:01 +0200

    commit initial

---
+++ src/main/java/org/jsystray/jsystray2/Jsystray2Application.java
@@ -0,0 +1,13 @@
+package org.jsystray.jsystray2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jsystray2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jsystray2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/jsystray/jsystray2/Jsystray2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jsystray.jsystray2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jsystray2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
